283a150d82195775399259ac472d100f439c5ea4
kirilx <angelov838@gmail.com>
2024-02-24 14:27:04 +0200

    Initial commit

---
+++ src/main/java/Car.java
@@ -0,0 +1,23 @@
+public class Car {
+    private String brand;
+    private String model;
+    private int year;
+    private double price;
+
+    // Constructor
+    public Car(String brand, String model, int year, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+    }
+    public int calculateAge() {
+        int currentYear = 2024 ;
+        return 2024 - year;
+
+    }
+    public double price() {
+        double currentear = 10500.000;
+        return 10500.000 - price;
+}
+}
