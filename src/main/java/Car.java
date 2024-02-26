6a6428a3d7abf0b8ebf236fbdbe97768023ed04a
kirilx <angelov838@gmail.com>
2024-02-26 11:40:05 +0200

    ee

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,23 +1,65 @@
+import java.time.Year;
+
 public class Car {
     private String brand;
     private String model;
     private int year;
     private double price;
 
-    // Constructor
+
     public Car(String brand, String model, int year, double price) {
         this.brand = brand;
         this.model = model;
         this.year = year;
         this.price = price;
     }
+
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
     public int calculateAge() {
-        int currentYear = 2024 ;
-        return 2024 - year;
+        int currentYear = Year.now().getValue();
+        return currentYear - year;
+    }
+
+    public double getTax() {
+        int age = calculateAge();
+        if (age >= 5) {
+            return price * 0.05;
+        } else {
+            return 0;
+        }
+    }
+
+
+    public void printInfo() {
+        System.out.println("Brand : " + brand);
+        System.out.println("Model : " + model);
+        System.out.println("Year: " + year);
+        System.out.println("Price: " + price);
+        System.out.println("Tax: " + getTax());
+        System.out.println("Age: " + calculateAge());
+    }
+    public Car() {
+        this.brand ="";
+        this.model ="";
+        this.year = 0;
+        this.price = 0.0;
+
 
     }
-    public double price() {
-        double currentear = 10500.000;
-        return 10500.000 - price;
-}
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        Car car = new Car("Ford", "Focus", 2022, 15000.0);
 
+        //Car car = new Car();
 
-
+        car.printInfo();
     }
 }
+
